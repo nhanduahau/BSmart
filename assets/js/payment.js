8838b2b88b46411102886f104072902cac876a12
213Horse <minhquando59@gmail.com>
2023-03-02 14:51:21 +0700

    fix them delete

--- assets/js/payment.js
+++ assets/js/payment.js
@@ -4,7 +4,7 @@ var count = 100;
 
 btnMomo.addEventListener('click', function() { 
     console.log('->', btnMomo)
-    
+
     var interval = setInterval(function() {
         countdown.innerHTML = count;
         count--;
@@ -12,3 +12,17 @@ btnMomo.addEventListener('click', function() {
     }, 1000);
 })
     
+
+// xóa khóa học
+const cards = document.querySelectorAll(".card1, .card2");
+
+for (let i = 0; i < cards.length; i++) {
+  const deleteButton = document.createElement("button");
+  deleteButton.textContent = "Xóa";
+  deleteButton.classList.add("card1-total-price-btn-delete");
+  deleteButton.addEventListener("click", () => {
+    cards[i].remove();
+  });
+  const cardTotalPrice = cards[i].querySelector(".card1-total-price-total-cost");
+  cardTotalPrice.appendChild(deleteButton);
+}
