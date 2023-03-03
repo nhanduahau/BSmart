a6b523f1d07830b87497f874ca695791e577519c
213Horse <minhquando59@gmail.com>
2023-03-03 12:33:03 +0700

    updated: fix payment.js

--- assets/js/payment.js
+++ assets/js/payment.js
@@ -32,4 +32,75 @@ deleteBtns.forEach((deleteBtn) => {
     card.remove();
   });
 });
+// đếm giây payment success
+var btnSuccess = document.getElementById('btn-success')
+var countdown = document.getElementById("countdown-1");
+var count = 10;
+btnSuccess.addEventListener('click', function() { 
+  console.log('->', btn-success)
+  var interval = setInterval(function() {
+    document.getElementById("countdown-1").innerHTML = count;
+    count--;
+    if (count < 0) {
+      clearInterval(interval);
+      // Thực hiện hành động khi đếm ngược kết thúc, ví dụ chuyển hướng trang
+      window.location.href = "";
+    }
+  }, 1000);
+})
 
+//   var btnSuccess = document.getElementById('btn-success')
+// var countdown = document.getElementById("countdown-1");
+// var count = 10;
+
+// btn-success.addEventListener('click', function() { 
+//     console.log('->', btn-success)
+
+//     var interval = setInterval(function() {
+//       document.getElementById("countdown-1").innerHTML = count;
+//         count--;
+//         if (count < 0) {
+//         clearInterval(interval);
+//          // Thực hiện hành động khi đếm ngược kết thúc, ví dụ chuyển hướng trang
+//       window.location.href = "https://vnexpress.net/";
+//         }
+//     }, 1000);
+// })
+
+
+// Điều kiện lúc thanh toán
+function checkBalance(e) {
+  e.preventDefault();
+
+  var balance = document.querySelector('.total-course-cash-balance').textContent.replace(/\./g, '').replace(' đ', '');
+  var payment = document.querySelector('.total-course-cash-balance-1').textContent.replace(/\./g, '').replace(' đ', '');
+  
+  if (parseInt(balance) < parseInt(payment)) {
+    alert("Không đủ tiền để thanh toán");
+    return false;
+  }
+  
+  // Thực hiện thanh toán nếu đủ tiền
+  window.location.href = "";
+}
+
+//tính toán số dư khi thanh toán
+// Lấy giá trị của hai phần tử HTML
+var balance1 = document.querySelector('.total-course-cash-balance').innerText;
+var payment1 = document.querySelector('.total-course-cash-balance-1').innerText;
+
+// Xử lý chuỗi và tính toán hiệu
+balance1 = parseInt(balance1.replace(/\D/g, ''));
+payment1 = parseInt(payment1.replace(/\D/g, ''));
+
+var remaining1 = balance1 - payment1;
+
+// Gán kết quả vào phần tử HTML
+document.querySelector('.total-course-cash-balance-2').innerText = remaining1.toLocaleString('vi-VN') + ' đ';
+
+
+//đếm item
+var parent = document.querySelector('.so-luong');
+var count = parent.querySelectorAll('.card-1').length;
+var countP = parent.querySelector('.count-1-1');
+countP.innerHTML = '(' + count + ') items';
