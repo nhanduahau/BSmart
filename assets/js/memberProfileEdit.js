0f1735b96b82b48cf998cc79fdf200a23639452c
tangbaotrann <tangbaotrann@gmail.com>
2023-03-09 11:35:20 +0700

    updated: fix

--- assets/js/memberProfileEdit.js
+++ assets/js/memberProfileEdit.js
@@ -1,3 +1,24 @@
+// validator images
+function previewAvatar() {
+  var optFile = document.getElementById("formFileAvatar");
+  var pathFile = optFile.value;
+  var allowedExtensions = /(\.jpg|\.jpeg|\.png)$/i;
+
+  if (!allowedExtensions.exec(pathFile)) {
+    alert("Vui lòng chọn file ảnh có đuôi là: .jpeg/.jpg/.png");
+    optFile.value = "";
+    return false;
+  } else {
+    avatarImage.src = "";
+    avatarImage.src = URL.createObjectURL(event.target.files[0]);
+  }
+}
+
+function clearAvatar() {
+  document.getElementById("formFileAvatar").value = null;
+  avatarImage.src = "./assets/images/avatar-mentor-1.jpg";
+}
+
 // điều kiện và ràng buộc khi nhập mật khẩu mới
 function checkPasswordMatch() {
   var password = document.getElementById("password");
@@ -30,18 +51,17 @@ function checkPasswordMatch() {
     passwordNotMatchFeedback.style.display = "none";
     passwordError.className = "invalid-feedback";
     return;
-}
-
+  }
 }
 function togglePasswordVisibility(id) {
   var input = document.getElementById(id);
   var icon = input.nextElementSibling.querySelector("span");
   if (input.type === "password") {
-      input.type = "text";
-      icon.textContent = "visibility_off";
+    input.type = "text";
+    icon.textContent = "visibility_off";
   } else {
-      input.type = "password";
-      icon.textContent = "visibility";
+    input.type = "password";
+    icon.textContent = "visibility";
   }
 }
 //thông báo ràng buộc mật khẩu
@@ -50,17 +70,18 @@ function validatePassword() {
   var passwordError = document.getElementById("password-error");
 
   if (password.validity.valid) {
-      passwordError.textContent = "";
-      passwordError.className = "invalid-feedback";
+    passwordError.textContent = "";
+    passwordError.className = "invalid-feedback";
   } else {
-      passwordError.textContent = "Mật khẩu mới phải có ít nhất 6 ký tự, bao gồm ít nhất 1 số, 1 chữ hoa, 1 ký tự đặc biệt";
-      passwordError.className = "invalid-feedback d-block";
+    passwordError.textContent =
+      "Mật khẩu mới phải có ít nhất 6 ký tự, bao gồm ít nhất 1 số, 1 chữ hoa, 1 ký tự đặc biệt";
+    passwordError.className = "invalid-feedback d-block";
   }
 }
 
 // Nút cập nhật phần mật khẩu
 function updateData(event) {
-    event.preventDefault(); // ngăn chặn chuyển trang mặc định của nút submit
-    // gửi yêu cầu cập nhật dữ liệu bằng AJAX
-    // ...
-  }
+  event.preventDefault(); // ngăn chặn chuyển trang mặc định của nút submit
+  // gửi yêu cầu cập nhật dữ liệu bằng AJAX
+  // ...
+}
