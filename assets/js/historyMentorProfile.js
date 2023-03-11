8be61cad890dbbe92107419325a993edf2df593f
tangbaotrann <tangbaotrann@gmail.com>
2023-03-11 14:42:24 +0700

    updated: page mentor-profile

---
+++ assets/js/historyMentorProfile.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $("#historyMentorProfile").DataTable({
+      searching: false,
+    });
+  
+    document.querySelector(".dataTables_length").style.display = "none";
+    document.querySelector(".dataTables_info").style.display = "none";
+  });
+  
