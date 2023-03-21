5960427f1f939e5665711d2834ad032fc1d1f244
tangbaotrann <tangbaotrann@gmail.com>
2023-03-21 22:12:22 +0700

    updated: bug-16; bug-6

--- assets/js/memberProfileEdit.js
+++ assets/js/memberProfileEdit.js
@@ -3,23 +3,26 @@ function previewAvatar() {
   var optFile = document.getElementById("formFileAvatar");
   var pathFile = optFile.value;
   var allowedExtensions = /(.jpg|.jpeg|.png)$/i;
-  
+
   if (!allowedExtensions.exec(pathFile)) {
-  alert("Vui lòng chọn file ảnh có đuôi là: .jpeg/.jpg/.png");
-  optFile.value = "";
-  return false;
+    alert("Vui lòng chọn file ảnh có đuôi là: .jpeg/.jpg/.png");
+    optFile.value = "";
+    return false;
   } else {
-  var fileSize = optFile.files[0].size; // lấy kích thước tệp đầu vào
-  if (fileSize > 2000000) { // giới hạn kích thước tệp là 2MB
-  alert("Kích thước tệp ảnh tải lên quá lớn. Vui lòng chọn tệp ảnh có dung lượng tối đa là 2MB.");
-  optFile.value = "";
-  return false;
-  } else {
-  avatarImage.src = "";
-  avatarImage.src = URL.createObjectURL(event.target.files[0]);
+    var fileSize = optFile.files[0].size; // lấy kích thước tệp đầu vào
+    if (fileSize > 2000000) {
+      // giới hạn kích thước tệp là 2MB
+      alert(
+        "Kích thước tệp ảnh tải lên quá lớn. Vui lòng chọn tệp ảnh có dung lượng tối đa là 2MB."
+      );
+      optFile.value = "";
+      return false;
+    } else {
+      avatarImage.src = "";
+      avatarImage.src = URL.createObjectURL(event.target.files[0]);
+    }
   }
-  }
-  }
+}
 
 function clearAvatar() {
   document.getElementById("formFileAvatar").value = null;
@@ -89,29 +92,54 @@ function validatePassword() {
 // Nút cập nhật phần mật khẩu
 function updateData(event) {
   event.preventDefault();
-  
+
   // Lưu thông tin vào localStorage
-  localStorage.setItem("avatarImage", document.getElementById("avatarImage").src);
-  localStorage.setItem("hoTen", document.querySelector('input[type="text"]').value);
-  localStorage.setItem("ngaySinh", document.querySelector('input[type="date"]').value);
-  localStorage.setItem("diaChi", document.querySelectorAll('input[type="text"]')[1].value);
-  localStorage.setItem("soDienThoai", document.querySelectorAll('input[type="text"]')[2].value);
+  localStorage.setItem(
+    "avatarImage",
+    document.getElementById("avatarImage").src
+  );
+  localStorage.setItem(
+    "hoTen",
+    document.querySelector('input[type="text"]').value
+  );
+  localStorage.setItem(
+    "ngaySinh",
+    document.querySelector('input[type="date"]').value
+  );
+  localStorage.setItem(
+    "diaChi",
+    document.querySelectorAll('input[type="text"]')[1].value
+  );
+  localStorage.setItem(
+    "soDienThoai",
+    document.querySelectorAll('input[type="text"]')[2].value
+  );
   localStorage.setItem("matKhauMoi", document.getElementById("password").value);
-  
+
   // Chuyển qua trang HTML khác
   window.location.href = "member-profile.html";
 }
 
-
-const inputDate = document.querySelector(".form-control");
-inputDate.addEventListener("blur", function() {
-  const dateString = this.value;
-  if (Date.parse(dateString)) {
-    console.log("Đây là kiểu ngày tháng năm hợp lệ!");
+// Validator DOB
+const inputDate = document.getElementById("dob-member-profile-edit");
+var patternDOB = /^([0-9]{2})-([0-9]{2})-([0-9]{4})$/;
+console.log(".", typeof patternDOB);
+inputDate.addEventListener("keyup", function () {
+  console.log("patternDOB.value", typeof inputDate.value);
+  if (patternDOB.test(inputDate.value)) {
+    console.log("valid!");
+    return true;
   } else {
-    console.log("Đây không phải là kiểu ngày tháng năm hợp lệ!");
-    // Hiển thị thông báo lỗi cho người dùng tại đây
+    console.log("invalid.");
+    return false;
   }
+
+  // const dateString = this.value;
+  // console.log("dateString", dateString);
+  // if (Date.parse(dateString)) {
+  //   console.log("Đây là kiểu ngày tháng năm hợp lệ!");
+  // } else {
+  //   console.log("Đây không phải là kiểu ngày tháng năm hợp lệ!");
+  //   // Hiển thị thông báo lỗi cho người dùng tại đây
+  // }
 });
-
-
