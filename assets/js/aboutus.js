e61fc719df0f6499704cd07dd93a635f4c39e8a2
213Horse <minhquando59@gmail.com>
2023-04-16 09:15:37 +0700

    fixbug

---
+++ assets/js/aboutus.js
@@ -0,0 +1,33 @@
+// type="text/javascript">
+//   $(document).ready(function(){
+//     $('.logo-frame').slick({
+//       infinite: true,
+//       slidesToShow: 5,
+//       slidesToScroll: 1,
+//       autoplay: true,
+//       autoplaySpeed: 0,
+//       speed: 5000,
+//       cssEase: 'linear',
+//       pauseOnHover: false,
+//       responsive: [
+//         {
+//           breakpoint: 992,
+//           settings: {
+//             slidesToShow: 4
+//           }
+//         },
+//         {
+//           breakpoint: 768,
+//           settings: {
+//             slidesToShow: 3
+//           }
+//         },
+//         {
+//           breakpoint: 576,
+//           settings: {
+//             slidesToShow: 2
+//           }
+//         }
+//       ]
+//     });
+//   });
