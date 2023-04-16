44fb8ad250d454bf8f4d5404b25027e8024ae437
Nguyen Xuan Tam <nguyenxuantam2611@gmail.com>
2023-04-17 00:34:57 +0700

    fix logo slider

--- assets/js/aboutus.js
+++ assets/js/aboutus.js
@@ -1,11 +1,11 @@
-const logos = document.querySelectorAll('.logo-grid img');
+const logos = document.querySelectorAll(".logo-grid img");
 let currentLogoIndex = 0;
 let numLogosToShow = 4;
 
 function showNextLogos() {
   // Ẩn số lượng logo hiện tại
   for (let i = currentLogoIndex; i < currentLogoIndex + numLogosToShow; i++) {
-    logos[i].style.display = 'none';
+    logos[i].style.display = "none";
   }
 
   // Tính toán chỉ số của số lượng logo tiếp theo
@@ -16,7 +16,11 @@ function showNextLogos() {
 
   // Hiển thị số lượng logo mới
   for (let i = currentLogoIndex; i < currentLogoIndex + numLogosToShow; i++) {
-    logos[i].style.display = 'block';
+    if (window.innerWidth >= 767) {
+      logos[i].style.display = "block";
+    } else {
+      logos[i].style.display = i === currentLogoIndex ? "block" : "none";
+    }
   }
 }
 
@@ -28,12 +32,11 @@ if (window.innerWidth >= 767) {
   numLogosToShow = 1;
   showNextLogos();
 }
-
 // Thực hiện hành động chuyển đổi số lượng logo sau mỗi 5 giây
 setInterval(showNextLogos, 3000);
 
 // Thay đổi số lượng logo tùy thuộc vào kích thước màn hình
-window.addEventListener('resize', function() {
+window.addEventListener("resize", function () {
   if (window.innerWidth >= 767) {
     numLogosToShow = 4;
     showNextLogos();
