dde61a3f391f0c1dd92b2dc79e1a08877cf81c9b
213Horse <minhquando59@gmail.com>
2023-04-29 01:17:42 +0700

    fixbug

--- assets/js/cousedetail.js
+++ assets/js/cousedetail.js
@@ -1,4 +1,21 @@
+// Lấy nút "lên đầu trang"
+var btnScrollToTop = document.querySelector("#btnScrollToTop");
 
+// Thêm sự kiện "scroll" vào trang web
+window.addEventListener("scroll", function() {
+  // Kiểm tra khi nào người dùng scroll xuống đủ để hiển thị nút "lên đầu trang"
+  if (window.pageYOffset > 100) {
+    btnScrollToTop.style.display = "block";
+  } else {
+    btnScrollToTop.style.display = "none";
+  }
+});
+
+// Thêm sự kiện "click" vào nút "lên đầu trang"
+btnScrollToTop.addEventListener("click", function() {
+  // Cuộn trang lên đầu
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
 
 // Lấy thông tin đăng nhập của người dùng
 const isLoggedIn = true; // Thay bằng true hoặc false tùy thuộc vào cách xác thực đăng nhập của bạn
