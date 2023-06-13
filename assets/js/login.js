f8656a9cf7842ac8b506277b9414fa41753f4a31
213Horse <minhquando59@gmail.com>
2023-06-13 18:41:59 +0700

    QUân sửa

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,13 +1,16 @@
 var message1ValidMatch = document.getElementById("message-validator1-match");
 
-function togglePasswordVisibility(id) {
-  var input = document.getElementById(id);
-  var icon = input.nextElementSibling.querySelector("span");
-  if (input.type === "password") {
-    input.type = "text";
-    // icon.textContent = "visibility_off";
+function togglePasswordVisibility(inputId) {
+  var passwordInput = document.getElementById(inputId);
+  var icon = passwordInput.nextElementSibling.firstElementChild;
+
+  if (passwordInput.type === "password") {
+    passwordInput.type = "text";
+    icon.classList.remove("fa-eye");
+    icon.classList.add("fa-eye-slash");
   } else {
-    input.type = "password";
-    // icon.textContent = "visibility";
+    passwordInput.type = "password";
+    icon.classList.remove("fa-eye-slash");
+    icon.classList.add("fa-eye");
   }
 }
